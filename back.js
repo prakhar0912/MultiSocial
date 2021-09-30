4fac5def344195de6db98d77d3aba59fdf25b5c5
prakhar <prakhar0912@gmail.com>
2021-09-30 19:09:37 +0530

    done

--- back.js
+++ back.js
@@ -1,37 +1,53 @@
-chrome.action.onClicked.addListener(function (tab) {
-    chrome.windows.create({
-        url: chrome.runtime.getURL("yay.html"),
-        type: "popup"
-    }, function (win) {
-        console.log(win)
-    });
-    xframehide()
+let windowId = null
+
+
+chrome.browserAction.onClicked.addListener(function (tab) {
+    if (windowId == null) {
+        chrome.windows.create({
+            url: chrome.runtime.getURL("index.html"),
+            type: "popup"
+        }, (wind) => {
+            windowId = wind.id
+        });
+    }
+    else {
+        chrome.windows.get(windowId, {}, (wind) => {
+            chrome.windows.update(wind.id, {
+                focused: true
+            })
+        })
+    }
+
 });
+
+chrome.windows.onRemoved.addListener(a => {
+    a === windowId && (windowId = null)
+})
+
 
 
 let xframehide = () => {
-    chrome.webRequest.onHeadersReceived.addListener(
-        function(info) {
-            var headers = info.responseHeaders;
-            for (var i=headers.length-1; i>=0; --i) {
-                var header = headers[i].name.toLowerCase();
-                if (header == 'x-frame-options' || header == 'frame-options') {
-                    headers.splice(i, 1); // Remove header
-                }
+    chrome.webRequest.onHeadersReceived.addListener((info) => {
+        var headers = info.responseHeaders;
+        for (var i = headers.length - 1; i >= 0; --i) {
+            var header = headers[i].name.toLowerCase();
+            if (header == 'x-frame-options' || header == 'frame-options' || header == 'x-frame-options') {
+                headers.splice(i, 1); // Remove header
             }
-            return {responseHeaders: headers};
-        }, {
-            urls: [
-                '*://*/*', // Pattern to match all http(s) pages
-                // '*://*.example.org/*', // Pattern to match one http(s) site
-            ], 
-            types: [ 'sub_frame' ]
-        }, [
-            'blocking',
-            'responseHeaders',
-            // Modern Chrome needs 'extraHeaders' to see and change this header,
-            // so the following code evaluates to 'extraHeaders' only in modern Chrome.
-            chrome.webRequest.OnHeadersReceivedOptions.EXTRA_HEADERS,
-        ].filter(Boolean)
-    );   
+        }
+        return { responseHeaders: headers };
+    }, {
+        urls: [
+            '*://*/*', // Pattern to match all http(s) pages
+            // '*://*.example.org/*', // Pattern to match one http(s) site
+        ],
+        types: ['sub_frame']
+    }, [
+        'blocking',
+        'responseHeaders',
+        // Modern Chrome needs 'extraHeaders' to see and change this header,
+        // so the following code evaluates to 'extraHeaders' only in modern Chrome.
+        chrome.webRequest.OnHeadersReceivedOptions.EXTRA_HEADERS
+    ]
+    );
 }
--- popup.js
+++ popup.js
@@ -4,12 +4,14 @@ let divs = document.querySelectorAll('div')
 let i = 0
 
 divs.forEach(div => div.style.display = 'none')
-    divs[i%2].style.display = 'block'
-    i++;
+divs[i%2].style.display = 'block'
+i++;
 
 but.addEventListener('click', () => {
     divs.forEach(div => div.style.display = 'none')
     divs[i%2].style.display = 'block'
+    but.innerHTML = i%2 == 1 ? 'Whatsapp' : 'Messenger'
     i++;
 
 })
+
